c95c5aa7f7e67aa072f5d5a45ed0e671c5834eaa
Documenter.jl <documenter@juliadocs.github.io>
2026-01-31 13:49:43 +0000

    build based on 465fdb2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MomentFittingQuadrature","page":"Home","title":"MomentFittingQuadrature","text":"","category":"section"},{"location":"#Index","page":"Home","title":"Index","text":"","category":"section"}]
+}
